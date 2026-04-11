18b8ee0d0480e322b738e30a0fc04ceb94aca983
AilenLopez <avlopez@fi.uba.ar>
2026-04-11 20:12:29 -0300

    Creando la Base de Datos =  BD_FIFA

---
+++ init_db.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE BD_FIFA;
+USE BD_FIFA;
+
+CREATE TABLE usuarios (
+ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(100) NOT NULL,
+email VARCHAR(100) NOT NULL UNIQUE
+)
+
+CREAT TABLE partidos (
+ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+equipo_local VACHAR(100) NOT NULL, 
+equipo_visitante VARCHAR(100) NOT NULL,
+fecha DATETIME NOT NULL,
+fase VARCHAR(50) NOT NULL,
+estadio: VARCHAR(50) NOT NULL,
+ciudad VARCHAR(50) NOT NULL,
+goles_local INT DEFAULT NULL, --todavía no se jugó
+goles_visitante INT DEFAULT NULL, 
+INDEX (fecha), -- pide filtrar partidos por equipo, fecha y fase
+INDEX (equipo_local),
+INDEX (equipo_visitante)
+)
+
+CREAT TABLE predicciones (
+ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+id_usuario INT NOT NULL,
+id_partido INT NOT NULL,
+goles_local INT NOT NULL,
+goles_visitante INT NOT NULL,
+FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE, -- no podes cargar una predicción para un id_usuario para un usuario que no existe 
+FOREIGN KEY (id_partido) REFERENCES partidos(id) ON DELETE CASCADE, -- si se elimina un usuario, se elimina su predicción
+UNIQUE KEY uq_unica_prediccion (id_usuario, id_partido) -- solo se permite una predicción por usuario por partido
+)
