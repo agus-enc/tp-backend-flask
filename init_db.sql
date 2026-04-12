7acc20b61a670a797b787983edb32f002b512fe3
agus-enc <aencalada@fi.uba.ar>
2026-04-11 21:35:29 -0300

    Creacion de init_db.py

--- init_db.sql
+++ init_db.sql
@@ -5,24 +5,24 @@ CREATE TABLE usuarios (
 ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 nombre VARCHAR(100) NOT NULL,
 email VARCHAR(100) NOT NULL UNIQUE
-)
+);
 
-CREAT TABLE partidos (
+CREATE TABLE partidos (
 ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-equipo_local VACHAR(100) NOT NULL, 
+equipo_local VARCHAR(100) NOT NULL,
 equipo_visitante VARCHAR(100) NOT NULL,
 fecha DATETIME NOT NULL,
 fase VARCHAR(50) NOT NULL,
-estadio: VARCHAR(50) NOT NULL,
+estadio VARCHAR(50) NOT NULL,
 ciudad VARCHAR(50) NOT NULL,
 goles_local INT DEFAULT NULL, --todavía no se jugó
 goles_visitante INT DEFAULT NULL, 
 INDEX (fecha), -- pide filtrar partidos por equipo, fecha y fase
 INDEX (equipo_local),
 INDEX (equipo_visitante)
-)
+);
 
-CREAT TABLE predicciones (
+CREATE TABLE predicciones (
 ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 id_usuario INT NOT NULL,
 id_partido INT NOT NULL,
@@ -31,4 +31,4 @@ goles_visitante INT NOT NULL,
 FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE, -- no podes cargar una predicción para un id_usuario para un usuario que no existe 
 FOREIGN KEY (id_partido) REFERENCES partidos(id) ON DELETE CASCADE, -- si se elimina un usuario, se elimina su predicción
 UNIQUE KEY uq_unica_prediccion (id_usuario, id_partido) -- solo se permite una predicción por usuario por partido
-)
+);
